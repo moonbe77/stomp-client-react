46930bab30edc0156a6ea3dc9beafaba3ed4d678
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2024-08-21 16:42:21 +1000

    update message

--- src/Events.tsx
+++ src/Events.tsx
@@ -2,36 +2,27 @@ import { useState } from "react";
 import { useStompClient, useSubscription } from "react-stomp-hooks";
 
 interface Payload {
-  ts: string;
-  chatId: number;
   event: "TYPING" | "NOT_TYPING";
+  name: string;
 }
 function Events({ chatId, url }: { chatId: number | null; url: string }) {
-  const [eventType, setEventType] = useState<string>("TYPING");
+  const [eventType, setEventType] = useState<Payload["event"]>("TYPING");
+  const [name, setName] = useState<string>("DEMO NAME");
   const [events, setEvents] = useState<Payload[]>([]);
   const stompClient = useStompClient();
 
   useSubscription(`/topic/chat/typing/${chatId}`, (message) => {
-    const payload: {
-      ts: string;
-      chatId: number;
-      event: "TYPING" | "NOT_TYPING";
-    } = JSON.parse(message.body);
-
-    console.log(message);
-    console.log(payload);
+    const payload: Payload = JSON.parse(message.body);
 
     setEvents((prev) => [...prev, payload]);
   });
 
   const handleSend = () => {
-    console.log("SEND", stompClient);
+    console.log("CLICK SEND", stompClient);
     if (stompClient) {
-      const now = new Date();
-      const payload = {
-        ts: now.toISOString(),
-        chatId,
+      const payload: Payload = {
         event: eventType,
+        name: name,
       };
 
       stompClient.publish({
@@ -58,21 +49,42 @@ function Events({ chatId, url }: { chatId: number | null; url: string }) {
             handleSend();
           }}
         >
-          <div className="row">
-            <label>EVENT to send TYPING | NOT_TYPING</label>
+          <div
+            className="form-group"
+            style={{
+              display: "flex",
+              gap: 4,
+            }}
+          >
+            <div className="row from-group">
+              <div className="col-sm-6">
+                <label>EVENT</label>
+                <br />
 
-            <select
-              className="form-control"
-              onChange={(e) => setEventType(e.target.value)}
-              value={eventType}
-            >
-              <option value="TYPING">TYPING</option>
-              <option value="NOT_TYPING">NOT TYPING</option>
-            </select>
+                <select
+                  className="form-control"
+                  onChange={(e) =>
+                    setEventType(e.target.value as Payload["event"])
+                  }
+                  value={eventType}
+                >
+                  <option value="TYPING">TYPING</option>
+                  <option value="NOT_TYPING">NOT TYPING</option>
+                </select>
+              </div>
+              <div className="col-sm-6">
+                <label>Name</label>
+                <br />
+                <input
+                  className="form-control"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  type="text"
+                />
+              </div>
+            </div>
           </div>
-          <code>
-            {`"ts":"timestamp now","chatId":${chatId},"event":"${eventType}"`}
-          </code>
+          <code>{`"chatId":${name},"event":"${eventType}"`}</code>
           <button id="send" className="btn btn-default" type="submit">
             Send
           </button>
@@ -84,9 +96,8 @@ function Events({ chatId, url }: { chatId: number | null; url: string }) {
           {events.map((event) => {
             return (
               <div>
+                <div>{event.name}</div>
                 <div>{event.event}</div>
-                <div>{event.chatId}</div>
-                <div>{event.ts}</div>
               </div>
             );
           })}
